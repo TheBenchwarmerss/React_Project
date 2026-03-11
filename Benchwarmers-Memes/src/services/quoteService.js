c79dacf129fc22913d280a087c839cb45f51092e
SamCodes21 <doerrsa2@msu.edu>
2026-03-11 16:11:01 +0000

    Better CORS proxy for API

--- Benchwarmers-Memes/src/services/quoteService.js
+++ Benchwarmers-Memes/src/services/quoteService.js
@@ -1,5 +1,5 @@
 export async function fetchQuotes(count = 3) {
-  const response = await fetch(`https://api.allorigins.win/raw?url=https://zenquotes.io/api/random/${count}`);
+  const response = await fetch(`https://corsproxy.io/?https://zenquotes.io/api/random/${count}`);
   const data = await response.json();
 
   return data.map(q => ({
