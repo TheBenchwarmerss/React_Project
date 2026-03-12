66096dbbfbc6da5ba531524a8e42e06ac24dfa25
Mannix Muir <155869270+Mannix-Codes@users.noreply.github.com>
2026-03-12 04:09:15 +0000

    fixed same issue problem

--- Benchwarmers-Memes/src/services/imageService.js
+++ Benchwarmers-Memes/src/services/imageService.js
@@ -3,7 +3,7 @@ export async function fetchDuckImages(count = 3) {
 
   for (let i = 0; i < count; i++) {
     requests.push(
-      fetch("https://corsproxy.io/?https://random-d.uk/api/v2/random")
+      fetch(`https://corsproxy.io/?https://random-d.uk/api/v2/random?v=${Date.now()}`)
       .then(res => res.json())
     );
   }
