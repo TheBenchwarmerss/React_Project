b084a8b61010fef90bc63e7a329dd7ce3e9474be
Mannix Muir <155869270+Mannix-Codes@users.noreply.github.com>
2026-03-15 04:15:19 +0000

    switched apis once again

--- Benchwarmers-Memes/netlify/functions/quotes.js
+++ Benchwarmers-Memes/netlify/functions/quotes.js
@@ -1,5 +1,5 @@
 export const handler = async (event) => {
-  const response = await fetch(`https://api.quotable.io/random`);
+  const response = await fetch(`https://stoic.tekloon.net/stoic-quote`);
   const data = await response.json();
   return {
     statusCode: 200,
