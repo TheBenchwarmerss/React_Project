93fbe656e94ff4a0f55c26937bc25b9c2fd3511a
Mannix Muir <155869270+Mannix-Codes@users.noreply.github.com>
2026-03-15 04:03:14 +0000

    switched quote api

--- Benchwarmers-Memes/netlify/functions/quotes.js
+++ Benchwarmers-Memes/netlify/functions/quotes.js
@@ -1,6 +1,6 @@
 export const handler = async (event) => {
   const count = event.queryStringParameters?.count || 3;
-  const response = await fetch(`https://zenquotes.io/api/random/${count}`);
+  const response = await fetch(`https://api.quotable.io/quotes`);
   const data = await response.json();
   return {
     statusCode: 200,
